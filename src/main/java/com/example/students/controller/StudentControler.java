51b6f387e1aa86bffff7135c0f002160414b07c4
dskushnir <kpds7175@gmail.com>
2019-04-05 09:33:43 +0300

    second commit

--- src/main/java/com/example/students/controller/StudentControler.java
+++ src/main/java/com/example/students/controller/StudentControler.java
@@ -14,17 +14,16 @@ public class StudentControler {
     private StudentService studentService;
     @Autowired
 
-
     public StudentControler(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @GetMapping("/students")
     public List<Student> getAllStudents() {
         return studentService.findAll();
     }
 
-
-  @PostMapping("/student")
+   @PostMapping("/student")
    public void createStudent(@RequestParam("firstName")String firstName,@RequestParam("lastName")String lastName){
         studentService.createStudent(firstName,lastName);
    }
--- src/main/java/com/example/students/repository/StudentRepository.java
+++ src/main/java/com/example/students/repository/StudentRepository.java
@@ -13,30 +13,26 @@ public class StudentRepository {
     private static Map<String, Student> studentIdToStudent = new HashMap<>();
     static {
         initStudents();
+
+
     }
 
     private static void initStudents() {
         Student student1 = new Student("01", "Ivan", "Ivanov");
         Student student2 = new Student("02", "Petr", "Petrov");
         Student student3 = new Student("03", "Jack","Born");
-        studentIdToStudent.put(student1.getId(),student1);
-        studentIdToStudent.put(student2.getId(),student2);
-        studentIdToStudent.put(student3.getId(),student3);
+        save(student1);
+        save(student2);
+        save(student3);
+
 
 
     }
-  /*  public Student getStudent(String id) {
-        return studentIdToStudent.get(id);
-    }*/
 
 
-
-    public void save(Student student) {
+    public static void save(Student student) {
         studentIdToStudent.put(student.getId(), student);
     }
-
-
-
 
 
     public List<Student> findAll() {
